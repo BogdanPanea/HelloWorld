92c030898117b5ad84f297c2d8857b6f5c903b8d
Bogdan <tombird83@yahoo.com>
2024-02-19 14:56:40 +0200

    Merge conflicts emergency-fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World ! Emergency fix.");
+    System.out.println("Hello World !");
+    System.out.println("Merge conflicts emergency-fix!");
   }
 }
