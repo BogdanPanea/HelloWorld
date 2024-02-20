62c99099904292c1c358e9b29c3c9b004f74c9e4
BogdanPanea <45076075+BogdanPanea@users.noreply.github.com>
2024-02-20 19:12:32 +0200

    GitHub new branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class Main {
     System.out.println("Hello World !");
     System.out.println("Merge conflicts emergency-fix!");
     System.out.println("Salut !");
+    System.out.println("GitHub new branch !");
  }
 }
